f2db3f4703c6dd45922486adeff58b8ac995440c
Giovani Machado <giovani.machado@gmail.com>
2020-04-12 15:00:09 -0300

    type: feat (add encryption with salt)
    Use bcrypt-nodejs package to imlemente the code. Bcrypt didn't work due to dependencies problems

--- app.js
+++ app.js
@@ -6,12 +6,15 @@
 // Level 2 - Encryption
 // Level 1 - Username and Password Only
 
-require("dotenv").config();
+require("dotenv").config(); // Implement enviromental variables
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encryption = require("mongoose-encryption");
+// const encryption = require("mongoose-encryption"); // Implement Level 2
+// const md5 = require("md5"); // Implement Level 3
+const bcrypt = require("bcrypt-nodejs");
+const saltRounds = 10;
 
 const app = express();
 
@@ -40,7 +43,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encryption,{secret: process.env.MYSECRET, encryptedFields: ["password"]});
+// userSchema.plugin(encryption,{secret: process.env.MYSECRET, encryptedFields: ["password"]}); // Implement Level 2
 
 const User = new mongoose.model("User", userSchema);
 
@@ -57,18 +60,22 @@ app.get("/login", function(req, res){
 app.post("/login", function(req, res){
   const username = req.body.username;
   const password = req.body.password;
-
-  User.findOne({email: username}, function(error, foundUser){
+  // const password = md5(req.body.password); // Level 3 implemantation
+  // const password = req.body.password; // Level 2 implemantation
+  User.findOne({email: username}, function(error, foundUser) {
     if (error){
       console.log(error);
     } else {
       if (foundUser){
-        if (foundUser.password === password){
-          res.render("secrets");
+        // if (foundUser.password === password){// Level 2 and 3
+          bcrypt.compare(req.body.password, foundUser.password, function(err, result){
+            if (result === true){
+              res.render("secrets");
+            }
+          });
         }
       }
-    }
-  });
+    });
 });
 
 app.get("/register", function(req, res){
@@ -78,18 +85,30 @@ app.get("/register", function(req, res){
 // Route to adress the POST request to be made by
 // form in register.ejs
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
+  console.log("Running Register Route");
+
+  bcrypt.genSalt(saltRounds, function(err, salt){
+      console.log(salt);
+      bcrypt.hash(req.body.password, salt, null, function(err, hash) { // Level 4 implementation
+      console.log("Running Bcrypt");
+      const newUser = new User({
+        email: req.body.username,
+        password: hash
+        // password: md5(req.body.password) // Level 3 implemantation
+        // password: req.body.password // Level 2 implemantation
+      });
+      console.log(hash);
+      newUser.save(function(err){
+        if (!err){
+          res.render("secrets");
+        } else {
+          console.log("Error:" + err);
+        }
+      });
+    });
   });
 
-  newUser.save(function(err){
-    if (!err){
-      res.render("secrets");
-    } else {
-      console.log(err);
-    }
-  });
+
 });
 
 app.listen(3000, function(){
