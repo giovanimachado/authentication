d98f91e24bbcddb4197dbffa2cb18cd4edcae880
Giovani Machado <giovani.machado@gmail.com>
2020-04-12 18:52:11 -0300

    type: refactor
    Remove comments related to old versions

--- app.js
+++ app.js
@@ -16,14 +16,9 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-// const encryption = require("mongoose-encryption"); // Level 2 implementation
-// const md5 = require("md5"); // Level 3 implementation
-// const bcrypt = require("bcrypt-nodejs"); // Level 4 implementation
-// const saltRounds = 10; // Level 4 implementation
 
 const app = express();
 
-// console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -38,22 +33,12 @@ app.use(passport.initialize());
 // use passport to manage sessions
 app.use(passport.session());
 
-
-
 mongoose.connect("mongodb://localhost:27017/userDB",{
   useNewUrlParser: true,
   useUnifiedTopology: true}
 );
 mongoose.set("useCreateIndex", true);
 
-// For level 1 auth, It is used a simple JS Object
-// const userSchema = {
-//   email: String,
-//   password: String
-// };
-
-// For level 2 auth, It is used a mongoose.Schema object
-// It is created form  mongoose.Schema class
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
@@ -74,15 +59,7 @@ const User = new mongoose.model("User", userSchema);
 // Use passport to create a local login stategy
 passport.use(User.createStrategy());
 
-
-// In the first attempt to use Oauth with google, I received a seiealized error
-// Create the session cookie using the package passport-local-mongoose
-// passport.serializeUser(User.serializeUser());
-// // Destroy (eat :) ) the session cookie
-// passport.deserializeUser(User.deserializeUser());
-
-
-// Srialize and deserialize using passport
+// Serialize and deserialize using passport
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
@@ -134,28 +111,6 @@ app.post("/login", function(req, res){
     }
   });
 });
-
-// Bellow here is the code for level 1 to 4 implemantation
-// app.post("/login", function(req, res){
-//   const username = req.body.username;
-//   const password = req.body.password;
-//   // const password = md5(req.body.password); // Level 3 implemantation
-//   // const password = req.body.password; // Level 2 implemantation
-//   User.findOne({email: username}, function(error, foundUser) {
-//     if (error){
-//       console.log(error);
-//     } else {
-//       if (foundUser){
-//         // if (foundUser.password === password){// Level 2 and 3
-//           bcrypt.compare(req.body.password, foundUser.password, function(err, result){
-//             if (result === true){
-//               res.render("secrets");
-//             }
-//           });
-//         }
-//       }
-//     });
-// });
 
 app.get("/register", function(req, res){
   res.render("register");
@@ -233,32 +188,6 @@ app.get("/logout", function(req, res){
   req.logout();
   res.redirect("/");
 });
-
-// Bellow here is the code for level 1 to 4 implemantation
-// app.post("/register", function(req, res){
-//   console.log("Running Register Route");
-//
-//   bcrypt.genSalt(saltRounds, function(err, salt){
-//       console.log(salt);
-//       bcrypt.hash(req.body.password, salt, null, function(err, hash) { // Level 4 implementation
-//       console.log("Running Bcrypt");
-//       const newUser = new User({
-//         email: req.body.username,
-//         password: hash
-//         // password: md5(req.body.password) // Level 3 implemantation
-//         // password: req.body.password // Level 2 implemantation
-//       });
-//       console.log(hash);
-//       newUser.save(function(err){
-//         if (!err){
-//           res.render("secrets");
-//         } else {
-//           console.log("Error:" + err);
-//         }
-//       });
-//     });
-//   });
-// });
 
 app.listen(3000, function(){
   console.log("Server running on port 3000");
